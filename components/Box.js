e4700cad56e58343dee3c7df296a8f4b6c4ce0aa
matveimug <madis.vaher@gmail.com>
2019-04-18 08:24:14 +0300

    scene more logic

---
+++ components/Box.js
@@ -0,0 +1,6 @@
+export default {
+    props: ['color'],
+    template: `
+        <a-box position="-1 0.5 -3" :color="color"></a-box>
+    `
+}
--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 import Layout from './components/Layout.js'
 import Scene from './components/Scene.js'
+import Box from './components/Box.js'
 
 new Vue({
     el: '#app',
-    components: { Layout, Scene },
+    components: { Layout, Scene, Box },
     template: `
     <Layout>
       <Scene>
-          <a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9"></a-box>
-          <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E"></a-sphere>
-          <a-cylinder position="1 0.75 -3" radius="0.5" height="1.5" color="#FFC65D"></a-cylinder>
+        <Box />
       </Scene>
     </Layout>
     `,
